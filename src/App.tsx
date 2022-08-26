561cfa1c04002111ddd4fcdb15e4c86a857d0dfd
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-26 18:02:53 +0100

    Initial folders

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React, {FC} from 'react';
+import './App.css';
+
+
+
+
+
+const App:FC = () => {
+
+
+
+  return (
+
+    <div className="App">
+    Hello
+    </div>
+
+  );
+}
+
+export default App;
