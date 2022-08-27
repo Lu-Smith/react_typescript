02d25cf39678eb535f6b330a437e756929a258aa
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-27 19:41:37 +0100

    Updated CowTypes.tsx
    Added CowTypes.css
    Moved changetheme from App.tsx to CowTypes.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,19 @@ import ChangeTheme from './app/components/ChangeTheme';
 import CowCollection from './app/components/CowCollection';
 import CowTypes from './app/components/CowTypes';
 import Footer from './app/components/Footer';
-import type { RootState } from './app/store';
-import { useSelector} from 'react-redux';
+
 
 
 const App:FC = () => {
 
-  const changetheme = useSelector((state: RootState) => state.changetheme.value);
+ 
+
   return (
     <div className="App">
       <ChangeTheme />
       <div className='main-content'>
-        <div className={changetheme}>
         <CowTypes />
-        </div>
-        <div className={changetheme}>
         <CowCollection />
-        </div>
       </div>
         <Footer />
     </div>
--- src/app/components/CowTypes.tsx
+++ src/app/components/CowTypes.tsx
@@ -1,8 +1,22 @@
-import React, {FC} from 'react'
+import React, {FC} from 'react';
+import type { RootState } from '../store';
+import { useSelector} from 'react-redux';
+import "../styles/CowTypes.css";
 
 const CowTypes: FC = () => {
+
+  const changetheme = useSelector((state: RootState) => state.changetheme.value);
+
   return (
-    <div>CowTypes</div>
+    <div className='CowTypes'>
+      <div className={changetheme}>
+         <h2>Type</h2>
+         <div>
+            <img src='https://lh3.googleusercontent.com/U76Rqh8XMN5mX69YHehblKxzDJYnXzM02DsOE8K2smcwQEZ5rs2a3p4VIJDsk5-p_oRjkVa62uzPsEmfJJxL10wOAT2xCV-AV7H207E=w600' alt='Flower Moo' />
+            <h5>Flower Moo</h5>
+         </div>
+      </div>
+    </div>
   )
 }
 
