4f91ebf93c9c37a74b83247c6eb5fa374a7e972f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-27 14:52:01 +0100

    Created changethemeSlice

---
+++ src/features/changethemeSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+
+export interface ChangeThemeState {
+  value: string
+}
+
+const initialState: ChangeThemeState = {
+  value: "",
+}
+
+export const changethemeSlice = createSlice({
+  name: 'changetheme',
+  initialState,
+  reducers: {
+    incrementByAmount: (state, action: PayloadAction<string>) => {
+      state.value = action.payload
+    },
+  },
+})
+
+export const { } = changethemeSlice.actions
+
+export default changethemeSlice.reducer
